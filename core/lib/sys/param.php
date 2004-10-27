f5cee1d8a9e4226659813da947ee3b759c7c1ad1
kpanic <kpanic@b5889315-3edd-0310-948c-aa339d9da174>
2004-10-27 13:31:57 +0000

    forgot to add param.php yesterday

---
+++ core/lib/sys/param.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  29o3 content management system
+  (c) 2003-2004 by Ulrik Guenther <kpanic@00t.org>
+  This software subjects to the license described in the
+  file LICENSE you should have received with this distribution.
+
+  System information
+*/
+
+/*
+	! @@@@ WARNING @@@@
+	!
+	! ALTERING THE FOLLOWING LINES MIGHT CAUSE 29o3 TO BEHAVE
+	! UNEXPECTEDLY. PLEASE LEAVE THEM AS THEY ARE UNLESS YOU
+	! HAVE A GOOD REASON TO CHANGE THEM.
+*/
+
+$SYSTEM_INFO = array(
+	"SystemVersion"		=>	"0.1 beta",
+	"SystemCodename"	=>	"Broend",
+	"SystemFeatures"	=>	"postgres",
+	"MinPhpVersion"		=>	"5.0.0"
+);
+
+?>
